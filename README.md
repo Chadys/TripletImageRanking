c78f44a51336129d1a65028b2eff036f5e8abff4
Julie Rymer <rymerjulie.pro@gmail.com>
2020-04-08 12:34:05 +0200

    load latest checkpoint, remove some deprecated arguments and save and evaluate model

---
+++ README.md
@@ -0,0 +1,3 @@
+An implementation of ["Learning Fine-grained Image Similarity with Deep Ranking"](https://arxiv.org/pdf/1404.4661.pdf) using Tensorflow 2.
+
+For now this implementation is still in progress (the loss is growing while training, there is still some work to do).
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# import os
+import os
 # os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'  # waiting for integration from tf-nvidia to tf
 import tensorflow as tf
 
@@ -16,11 +16,13 @@ IMG_SHAPE = (
     if tf.keras.backend.image_data_format() == "channels_last"
     else (3, IMG_SIZE, IMG_SIZE)
 )
-EPOCHS = 2
+EPOCHS = 5
 GAP_PARAMETER = 0.2
 EMBEDDINGS_DIM = 4096
 BATCH_SIZE = data.BATCH_SIZE
 
+CHECKPOINT_PATH = "saves/weights.{epoch:02d}-{val_loss:.2f}.ckpt"
+CHECKPOINT_DIR = os.path.dirname(CHECKPOINT_PATH)
 
 # strategy = tf.distribute.MirroredStrategy()
 # print("Number of devices: {}".format(strategy.num_replicas_in_sync))
@@ -30,6 +32,7 @@ BATCH_SIZE = data.BATCH_SIZE
 # TODO add activation layers ?
 
 print(f"image data format is {tf.keras.backend.image_data_format()}")
+latest_ckpt = tf.train.latest_checkpoint(CHECKPOINT_DIR)
 datasets = data.get_hard_images()
 model = DeepRankingNetwork(IMG_SHAPE, EMBEDDINGS_DIM)
 base_lr = 0.001
@@ -40,17 +43,24 @@ model.compile(
     metrics=[],
 )
 
-steps_per_epoch = datasets["train"][0] // BATCH_SIZE
+if latest_ckpt is not None:
+    model.load_weights(latest_ckpt)
+
 model.fit(
     datasets["train"][1],
     validation_data=datasets["validation"][1],
     validation_steps=datasets["validation"][0] // BATCH_SIZE,
-    steps_per_epoch=steps_per_epoch,
+    steps_per_epoch=datasets["train"][0] // BATCH_SIZE,
     epochs=EPOCHS,
     callbacks=[
         tf.keras.callbacks.LearningRateScheduler(schedule=lambda epoch_index: base_lr + (max_lr-base_lr)*(max(0, (1-epoch_index))*1/(2.**(epoch_index-1)))),  # TODO test w/ ReduceLROnPlateau instead
         tf.keras.callbacks.EarlyStopping("val_loss", patience=10, restore_best_weights=True),
-        tf.keras.callbacks.ModelCheckpoint(filepath="saves/weights.{epoch:02d}-{val_loss:.2f}.hdf5", monitor="val_loss", save_best_only=True, save_freq="epoch"),
-        tf.keras.callbacks.TensorBoard(histogram_freq=5, batch_size=BATCH_SIZE, write_grads=True, write_images=True)
+        tf.keras.callbacks.ModelCheckpoint(filepath=CHECKPOINT_PATH, monitor="val_loss", save_best_only=True, save_freq="epoch", verbose=1, save_weights_only=True),
+        tf.keras.callbacks.TensorBoard(histogram_freq=5, write_images=True)
     ]
 )
+
+loss = model.evaluate(datasets["test"][1], steps=datasets["test"][0] // BATCH_SIZE, verbose=2)
+print(f'loss: {loss}')
+
+model.save('deep_ranking_model', save_format="tf")
